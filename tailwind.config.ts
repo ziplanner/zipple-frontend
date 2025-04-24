25ac4369caae4470bfdac778df8c98b4405e221f
yeoniii20 <commetoi_yeoni@naver.com>
2025-04-24 23:01:07 +0900

    chore: Add tailwind css

---
+++ tailwind.config.ts
@@ -0,0 +1,49 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ["Pretendard", "sans-serif"],
+      },
+      colors: {
+        main: "#3772F7",
+        sub: "#FFE100",
+        skyblue: "#AFC8FF",
+        error: "#FF4D00",
+        text: {
+          primary: "#222222",
+          secondary: "#767676",
+          light: "#949494",
+          menu: "#505050",
+        },
+        background: {
+          light: "#DDDDDD",
+          soft: "#F5F5F5",
+          extraSoft: "#F6F6F9",
+        },
+      },
+      boxShadow: {
+        default: "2px 2px 12px 0px rgba(0, 0, 0, 0.12)",
+        border: "0px -3px 20px 0px rgba(0, 0, 0, 0.05)",
+        modal: "0px -6px 30px 0px rgba(0, 0, 0, 0.08)",
+      },
+      maxWidth: {
+        "screen-xl2": "1920px",
+      },
+      screens: {
+        sm: "360px",
+        md: "768px",
+        lg: "1024px",
+        lx: "1288px",
+      },
+      fontSize: {},
+    },
+  },
+  plugins: [],
+} satisfies Config;
