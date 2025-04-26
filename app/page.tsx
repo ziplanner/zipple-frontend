237200325b08e3da68f69c92118adfba4ab1068e
yeoniii20 <commetoi_yeoni@naver.com>
2025-04-26 23:51:26 +0900

    feat: Add Pretendard fonts

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Input
+      {/* <Input
         value={inputValue}
         onChange={handleInputChange}
         error={true}
         errorMessage="다시 입력해주세요."
-      />
+      /> */}
     </div>
   );
 }
