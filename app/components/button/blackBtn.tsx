a18d79f0e18298083a9489f4ab1f1346596bc1f8
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-28 16:35:41 +0900

    style: Update blackBtn.tsx

--- app/components/button/blackBtn.tsx
+++ app/components/button/blackBtn.tsx
@@ -3,8 +3,8 @@ import { ButtonProps } from "./primaryBtn";
 export const BlackBtn = ({ text, className = "", onClick }: ButtonProps) => (
   <button
     onClick={onClick}
-    className={`h-[48px] px-[36px] rounded-[6px] bg-btn text-white
-        text-18s ${className}`}
+    className={`h-10 md:h-[48px] px-[36px] rounded-[6px] bg-btn text-white
+        text-14m md:text-18s ${className}`}
   >
     {text}
   </button>
