bdc9a8de9198deb35d20d9300642c0ec2579d401
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-28 16:35:53 +0900

    style: Update blueBtn.tsx

--- app/components/button/blueBtn.tsx
+++ app/components/button/blueBtn.tsx
@@ -3,8 +3,8 @@ import { ButtonProps } from "./primaryBtn";
 export const BlueBtn = ({ text, className = "", onClick }: ButtonProps) => (
   <button
     onClick={onClick}
-    className={`h-[42px] px-[24px] rounded-[6px] bg-main text-white
-        text-16s ${className}`}
+    className={`h-9 md:h-[42px] px-[24px] rounded-[6px] bg-main text-white
+        text-14m md:text-16s ${className}`}
   >
     {text}
   </button>
