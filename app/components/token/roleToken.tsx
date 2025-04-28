962c570bfa3ceb6395f70eb0bb0c578f65a781d8
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-28 18:14:12 +0900

    design: Create token and menu components

---
+++ app/components/token/roleToken.tsx
@@ -0,0 +1,24 @@
+interface RoleTokenProps {
+  role: "GENERAL" | "REPRESENTATION" | "ASSOCIATE" | "EXPERT" | "NONE";
+}
+
+const roleNames: Record<RoleTokenProps["role"], string> = {
+  GENERAL: "일반",
+  REPRESENTATION: "대표 공인중개사",
+  ASSOCIATE: "소속 공인중개사",
+  EXPERT: "생활 전문가",
+  NONE: "",
+};
+
+const RoleToken = ({ role }: RoleTokenProps) => {
+  return (
+    <div
+      className="inline-flex justify-center items-center text-14b
+    border border-main px-3 py-[1px] text-main rounded-full"
+    >
+      {roleNames[role]}
+    </div>
+  );
+};
+
+export default RoleToken;
