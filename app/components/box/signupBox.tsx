fb90b2ca7bd385bb7f64854503efd11a4f1e7f91
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-28 16:33:53 +0900

    design: Create termsBox component

--- app/components/box/signupBox.tsx
+++ app/components/box/signupBox.tsx
@@ -45,41 +45,48 @@ const SignupBox = ({ type, isSelected, onSelect }: SignupBoxProps) => {
   return (
     <div
       className={`flex flex-col items-center group border rounded-[20px] transition-all duration-300 ease-in-out
-        w-[380px] h-[460px] cursor-pointer justify-center
+        lx:w-[380px] lx:h-[460px] lg:w-[300px] lg:h-[380px] md:w-[240px] md:h-[320px] max-w-[400px]
+        cursor-pointer justify-center
         ${
           isSelected
-            ? "bg-main border-none scale-105"
+            ? "bg-main border-none md:scale-105"
             : "border-background-light"
         }
         hover:scale-105 hover:bg-main hover:border-none hover:shadow-lg`}
       onClick={handleCardClick}
     >
-      <Image
-        src={typeToImg[type]}
-        alt={type}
-        width={80}
-        height={80}
-        className="transition-all duration-300 group-hover:brightness-0 group-hover:invert mb-5"
-      />
-      <p
-        className={`text-text-primary text-18r transition-all duration-300 group-hover:text-white my-5`}
-      >
-        {typeToDesc[type]}
-      </p>
-      <div className="flex w-full justify-center">
-        {isSelected ? (
-          <WhiteBtn
-            onClick={handleCardClick}
-            text={`${type} 회원 인증`}
-            className="group-hover:bg-white group-hover:text-main"
-          />
-        ) : (
-          <BlackBtn
-            onClick={handleCardClick}
-            text={`${type} 회원 인증`}
-            className="group-hover:bg-white group-hover:text-main"
-          />
-        )}
+      <div className="flex w-full justify-center gap-7 md:gap-0 flex-row md:flex-col items-center">
+        <Image
+          src={typeToImg[type]}
+          alt={type}
+          width={40}
+          height={40}
+          className="transition-all duration-300 group-hover:brightness-0 group-hover:invert mb-0 md:mb-5
+        lg:w-20 lg:h-20 md:w-16 md:h-16"
+        />
+        <div className="flex flex-col items-center">
+          <p
+            className={`text-text-primary text-14r md:text-16r lg:text-18r transition-all duration-300
+              group-hover:text-white mt-6 mb-2.5 md:my-5`}
+          >
+            {typeToDesc[type]}
+          </p>
+          <div className="flex mb-6 md:mb-0 md:w-full justify-center w-[190px]">
+            {isSelected ? (
+              <WhiteBtn
+                onClick={handleCardClick}
+                text={`${type} 회원 인증`}
+                className="group-hover:bg-white group-hover:text-main"
+              />
+            ) : (
+              <BlackBtn
+                onClick={handleCardClick}
+                text={`${type} 회원 인증`}
+                className="group-hover:bg-white group-hover:text-main"
+              />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
