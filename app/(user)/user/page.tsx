302fc1a4167c3b65d9c35fd7f92d82d10a0c8656
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-28 18:14:43 +0900

    feat: Create user page

---
+++ app/(user)/user/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ProfileSection from "./components/profileSection";
+
+const UserPage = () => {
+  return (
+    <div>
+      <ProfileSection />
+    </div>
+  );
+};
+
+export default UserPage;
