f71ad386b45f1cdd4590cec5b1ff5368858059f0
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-05-02 17:06:12 +0900

    feat: Connect url

--- app/components/layout/header/navMenu.tsx
+++ app/components/layout/header/navMenu.tsx
@@ -1,21 +1,31 @@
+import { usePathname, useRouter } from "next/navigation";
+
+const menus = [
+  { label: "중개사매칭", url: "/agent" },
+  { label: "생활서비스", url: "/service" },
+  { label: "집플래너", url: "/planner" },
+  { label: "커뮤니티", url: "/community" },
+  { label: "고객안내", url: "/help" },
+];
+
 const NavMenu = () => {
-  const menus = [
-    "중개사매칭",
-    "생활서비스",
-    "집플래너",
-    "커뮤니티",
-    "고객안내",
-  ];
+  const router = useRouter();
+  const pathnmame = usePathname();
 
   return (
     <nav className="flex lg:space-x-10 md:space-x-5">
       {menus.map((menu) => (
         <span
-          key={menu}
-          className={`cursor-pointer text-text-primary hover:text-main 
-            lg:text-18m md:text-14m`}
+          key={menu.label}
+          className={`cursor-pointer hover:text-main 
+            lg:text-18m md:text-14m ${
+              pathnmame === menu.url ? "text-main" : "text-text-primary"
+            }`}
+          onClick={() => {
+            router.push(menu.url);
+          }}
         >
-          {menu}
+          {menu.label}
         </span>
       ))}
     </nav>
