93cb58f11dc670dcf54bcb03494d0b005e552e0d
yeoniii20 <commetoi_yeoni@naver.com>
2025-05-06 23:55:13 +0900

    feat: Add api urls

---
+++ app/api/apiUrl.ts
@@ -0,0 +1,13 @@
+// login
+export const LOGIN = "/api/v2/login/kakao";
+export const LOGOUT = "/api/v2/login/logout";
+export const RENEW = "/api/v2/login/renew";
+export const ROLE = "/api/v2/login/role";
+export const WITHDRAW = "/api/v2/login/withdraw";
+export const WITHDRAW_USERTYPE = "/api/v2/login/withdraw/usertype";
+
+// register
+export const REGISTER_ASSOCIATE = "/api/v2/register/associate";
+export const REGISTER_EXPERT = "/api/v2/register/expert";
+export const REGISTER_GENERAL = "/api/v2/register/general";
+export const REGISTER_REPRESENTATIVE = "/api/v2/register/representative";
