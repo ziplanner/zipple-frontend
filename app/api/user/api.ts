4e160cc90e4f6f4b03665aa9412eb712dfa7921a
yeoniii20 <commetoi_yeoni@naver.com>
2025-05-08 01:06:34 +0900

    feat: Add mypage api

--- app/api/user/api.ts
+++ app/api/user/api.ts
@@ -14,6 +14,14 @@ interface GeneralUserResponse {
   mainEmail: string;
 }
 
+interface GeneralUserRequest {
+  nickname: string;
+  phoneNumber: string;
+  address: string;
+  houseType: string;
+  mainEmail: string;
+}
+
 interface RepresentativeUserResponse {
   phoneNumber: string;
   mainEmail: string;
@@ -25,6 +33,24 @@ interface RepresentativeUserResponse {
   specializedType: string;
 }
 
+interface RepresentativeUserRequest {
+  phoneNumber: string;
+  mainEmail: string;
+  introduceUrl: string;
+  representativeArea: string[];
+  additionalArea: string[];
+  introduceTitle: string;
+  introduceContent: string;
+}
+
+interface AssociateUserRequest {
+  phoneNumber: string;
+  mainEmail: string;
+  introduceUrl: string;
+  introduceTitle: string;
+  introduceContent: string;
+}
+
 interface AssociateUserResponse {
   phoneNumber: string;
   mainEmail: string;
@@ -32,6 +58,13 @@ interface AssociateUserResponse {
   introduceTitle: string;
   introduceContent: string;
   specializedType: string;
+}
+
+interface ExpertUserRequest {
+  phoneNumber: string;
+  mainEmail: string;
+  introduceTitle: string;
+  introduceContent: string;
 }
 
 interface ExpertUserResponse {
@@ -49,10 +82,24 @@ export const getGeneralUserRole = async (): Promise<GeneralUserResponse> => {
   return response.data;
 };
 
+// 일반 회원정보 수정
+export const updateGeneralUserRole = async (
+  data: GeneralUserRequest
+): Promise<void> => {
+  await axiosInstance.put(MYPAGE_GENERAL, data);
+};
+
 // 대표 회원정보 조회
 export const getRepUserRole = async (): Promise<RepresentativeUserResponse> => {
   const response = await axiosInstance.get(MYPAGE_REPRESENTATIVE);
   return response.data;
+};
+
+// 대표 회원정보 수정
+export const updateRepUserRole = async (
+  data: RepresentativeUserRequest
+): Promise<void> => {
+  await axiosInstance.put(MYPAGE_REPRESENTATIVE, data);
 };
 
 // 소속 회원정보 조회
@@ -62,8 +109,22 @@ export const getAssociateUserRole =
     return response.data;
   };
 
+// 소속 회원정보 조회
+export const updateAssociateUserRole = async (
+  data: AssociateUserRequest
+): Promise<void> => {
+  await axiosInstance.put(MYPAGE_ASSOCIATE, data);
+};
+
 // 생활전문가 회원정보 조회
 export const getExpertUserRole = async (): Promise<ExpertUserResponse> => {
   const response = await axiosInstance.get(MYPAGE_EXPERT);
   return response.data;
+};
+
+// 생활전문가 회원정보 수정
+export const updateExpertUserRole = async (
+  data: ExpertUserRequest
+): Promise<void> => {
+  await axiosInstance.put(MYPAGE_EXPERT, data);
 };
