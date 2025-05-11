7c7f2fd00a47c754a4f8dd43608be489a85a819b
yeoniii20 <commetoi_yeoni@naver.com>
2025-05-12 00:43:14 +0900

    feat: Create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
 "use client";
 
-import { useState } from "react";
-import Input from "./components/input/input";
+import MainSection from "./(home)/content/mainSection";
+import { SegmentedTab } from "./components/tab/segemtedTab";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState("zzzzz");
-
-  const handleInputChange = (event: any) => {
-    setInputValue(event.target.value);
-  };
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <Input
-        value={inputValue}
-        onChange={handleInputChange}
-        error={true}
-        errorMessage="다시 입력해주세요."
-      /> */}
+    <div
+      className="bg-white w-full flex flex-col items-center py-[120px]
+    md:max-w-screen-xl2 px-4 lx:px-20"
+    >
+      <h1 className="text-text-primary md:text-48s text-36s">
+        어떤 서비스를 찾으시나요?
+      </h1>
+      <p className="text-text-secondary md:text-20r text-18r mt-5 mb-16 md:mb-20">
+        매물 찾기부터 이사 후 정착까지, 집에 관한 모든 전문가를 한 곳에서
+      </p>
+      <SegmentedTab
+        tabs={["중개사매칭", "주요 서비스", "생활서비스"]}
+        defaultTab="주요 서비스"
+        onChange={(tab) => console.log("선택된 탭:", tab)}
+        className="md:mb-[60px] mb-12"
+      />
+      <MainSection />
     </div>
   );
 }
