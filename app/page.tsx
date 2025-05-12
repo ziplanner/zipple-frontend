c104f7a7cc5c9cf0fd2af5bf2a01c0b5a2b5e914
yeoniii20 <commetoi_yeoni@naver.com>
2025-05-12 23:27:10 +0900

    feat: Add renderSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
 "use client";
 
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import AgentSection from "./(home)/content/agentSection";
+import ExpertSection from "./(home)/content/expertSection";
 import MainSection from "./(home)/content/mainSection";
 import { SegmentedTab } from "./components/tab/segemtedTab";
 
 export default function Home() {
+  const [selectedTab, setSelectedTab] = useState<string>("주요 서비스");
+
+  const renderSection = () => {
+    switch (selectedTab) {
+      case "중개사매칭":
+        return <AgentSection key="중개사매칭" />;
+      case "주요 서비스":
+        return <MainSection key="주요 서비스" />;
+      case "생활서비스":
+        return <ExpertSection key="생활서비스" />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div
       className="bg-white w-full flex flex-col items-center py-[120px]
-    md:max-w-screen-xl2 px-4 lx:px-20"
+      md:max-w-screen-xl2 px-4 lx:px-20"
     >
       <h1 className="text-text-primary md:text-48s text-36s">
         어떤 서비스를 찾으시나요?
@@ -18,10 +37,22 @@ export default function Home() {
       <SegmentedTab
         tabs={["중개사매칭", "주요 서비스", "생활서비스"]}
         defaultTab="주요 서비스"
-        onChange={(tab) => console.log("선택된 탭:", tab)}
+        onChange={(tab) => setSelectedTab(tab)}
         className="md:mb-[60px] mb-12"
       />
-      <MainSection />
+
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={selectedTab}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.25 }}
+          className="w-full"
+        >
+          {renderSection()}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
