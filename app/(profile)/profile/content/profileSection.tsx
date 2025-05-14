967bbd5b93d63952853136ea8cbec3be679641d2
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-05-14 14:20:13 +0900

    style: Update profileSection.tsx

--- app/(profile)/profile/content/profileSection.tsx
+++ app/(profile)/profile/content/profileSection.tsx
@@ -1,73 +1,84 @@
-import { BasicBtn } from "@/app/components/button/basicBtn";
-import { PortfolioCard } from "@/app/components/card/portfolioCard";
-import Pagination from "@/app/components/pagination/pagination";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Image from "next/image";
+import defaultProfile from "@/app/images/icon/default_profile.svg";
+import phone from "@/app/images/icon/phone.svg";
+import RoleToken from "@/app/components/token/roleToken";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useBrokerDetail } from "@/app/context/agentDetailContext";
+import { formatPhoneNumber } from "@/app/utils/phoneNumber";
 
-const DUMMY_DATA = Array(33)
-  .fill(0)
-  .map((_, i) => ({
-    id: i + 1,
-    title: `(포트폴리오 ${
-      i + 1
-    }) 100평대 사무실 임대 최대 한줄까지만 들어가겠습니다! `,
-    date: "2025.05.05",
-  }));
+const ProfileSection = () => {
+  const data = useBrokerDetail();
 
-const ITEMS_PER_PAGE = 12;
-
-const PortfolioSection = () => {
-  const router = useRouter();
-
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [open, setOpen] = useState<boolean>(false);
-  const [showAlert, setShowAlert] = useState<boolean>(false);
-
-  const startIdx = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIdx = startIdx + ITEMS_PER_PAGE;
-  const currentItems = DUMMY_DATA.slice(startIdx, endIdx);
-
-  const totalPages = Math.ceil(DUMMY_DATA.length / ITEMS_PER_PAGE);
+  const [liked, setLiked] = useState<boolean>(false);
 
   return (
     <div
-      className="flex w-full flex-col md:px-8 md:py-10 lg:px-[60px] lg:py-20
-    px-[15px]"
+      className="md:w-[280px] lx:w-[340px] md:pt-5 md:px-12 lx:py-10 lx:px-[60px]
+    border border-border rounded-[20px] md:ml-8 lg:ml-20 md:mt-10 lg:mt-20"
     >
-      <div className="flex justify-between w-full mb-10">
-        <h2 className="text-text-primary text-18s md:text-24s">포토폴리오</h2>
-        <BasicBtn
-          onClick={() => {
-            router.push("/profile");
-          }}
-          text={"전체보기"}
-        />
-      </div>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 md:gap-6 lg:gap-8 mb-[60px] md:mb-20">
-        {currentItems.map((item) => (
-          <PortfolioCard
-            key={item.id}
-            title={item.title}
-            date={item.date}
-            portfolioId={0}
-            onEdit={function (id: number): void {
-              throw new Error("Function not implemented.");
-            }}
-            onDelete={function (id: number): void {
-              throw new Error("Function not implemented.");
-            }}
+      <div className="flex flex-col w-full items-center">
+        <div className="md:w-[180px] md:h-[180px] lx:w-[220px] lx:h-[220px] rounded-full overflow-hidden">
+          <Image
+            src={data?.profileUrl || defaultProfile}
+            alt="User"
+            width={180}
+            height={180}
+            className="object-cover w-full h-full md:w-[180px] md:h-[180px] lx:w-[220px] lx:h-[220px]"
           />
-        ))}
+        </div>
+        <p className="text-text-primary text-24m mb-[6px] mt-5">
+          {data?.nickname}
+        </p>
+        <RoleToken role={data?.roleName || "GENERAL"} />
+        {/* Badges */}
+        {/* <div className="flex flex-wrap justify-end gap-2 mt-5">
+          {["대표", "1인 가구 전문가"].map((badge, i) => (
+            <span
+              key={i}
+              className="bg-yellow-100 text-yellow-700 text-12 m px-2 py-0.5 rounded-md"
+            >
+              {badge}
+            </span>
+          ))}
+        </div> */}
+        {/* 핸드폰 번호 */}
+        <button
+          className="flex w-full items-center justify-center py-2.5 gap-1.5 
+          rounded-md border border-main mt-[74px] text-nowrap"
+          onClick={() => {}}
+        >
+          <Image
+            src={phone}
+            alt={"phone"}
+            width={20}
+            height={20}
+            className="md:w-5 md:h-5"
+          />
+          <p className="text-main text-18m">
+            {data?.phoneNumber
+              ? formatPhoneNumber(data.phoneNumber)
+              : "번호 비공개"}
+          </p>
+        </button>
+        {/* Like Button */}
+        <div className="flex flex-col items-center justify-center mt-5">
+          {liked ? (
+            <FaHeart className="text-error text-[26px]" />
+          ) : (
+            <FaRegHeart className="text-text-light text-[26px]" />
+          )}
+          <span
+            className={`text-12m lg:text-14m ${
+              data?.likesCount || 0 > 0 ? "text-error" : "text-text-light"
+            }`}
+          >
+            {data?.likesCount}
+          </span>
+        </div>
       </div>
-      {/* Pagination */}
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={setCurrentPage}
-        className="mb-24 md:mb-[120px]"
-      />
     </div>
   );
 };
 
-export default PortfolioSection;
+export default ProfileSection;
