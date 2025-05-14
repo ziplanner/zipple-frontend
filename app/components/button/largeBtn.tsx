59334993af400519ccb76dd0c8cc224b4d368279
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-05-14 14:17:05 +0900

    style: Add colors

--- app/components/button/largeBtn.tsx
+++ app/components/button/largeBtn.tsx
@@ -14,7 +14,9 @@ export const LargeBtn = ({
           disabled
             ? color === "black"
               ? "bg-btn40 text-white"
-              : "bg-main40 text-white"
+              : color === "blue"
+              ? "bg-main40 text-white"
+              : "border text-[#a7a7a7] border-[#a7a7a7]"
             : color === "black"
             ? "bg-btn text-white"
             : color === "blue"
@@ -23,6 +25,8 @@ export const LargeBtn = ({
             ? "bg-white text-text-secondary"
             : color === "gray"
             ? "bg-white border border-text-secondary text-text-secondary"
+            : color === "borderBlue"
+            ? "border border-main bg-white text-main"
             : "bg-white border border-black text-black"
         } ${className}
 `}
