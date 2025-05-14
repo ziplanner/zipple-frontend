b963fce9f66aef35d9e9f8ce7a1b515503e8538c
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-05-14 14:21:06 +0900

    feat: Create scrap section

--- app/(home)/content/regionSelectSection.tsx
+++ app/(home)/content/regionSelectSection.tsx
@@ -23,39 +23,39 @@ const regions = [
 ];
 
 const RegionSelectSection = () => {
-  const [selectedTab, setSelectedTab] = useState("공인중개사");
-  const [selectedRegion, setSelectedRegion] = useState("경기");
+  const [selectedTab, setSelectedTab] = useState<string>("공인중개사");
+  const [selectedRegion, setSelectedRegion] = useState<string>("경기");
 
   return (
     <section
       className="w-full bg-background-extraSoft py-[60px] md:py-[100px]
     flex justify-center"
     >
-      <div className="w-full md:px-4 lx:px-20 md:max-w-screen-xl2">
+      <div className="w-full px-[15px] lx:px-20 md:max-w-screen-xl2">
         {/* 타이틀 + 탭 */}
         <div
           className="flex flex-col gap-3 md:gap-0 md:flex-row justify-between 
         md:items-start items-center mb-10 md:mb-14"
         >
-          <h2 className="text-24m md:text-32m text-text-primary">
+          <h2 className="text-24m md:text-36m text-text-primary text-center">
             어떤 지역의 <span className="text-main">중개사</span>를 찾으시나요?
           </h2>
           <SegmentedTab
             tabs={["공인중개사", "생활서비스 전문가"]}
             defaultTab={selectedTab}
             onChange={setSelectedTab}
-            className="bg-white"
+            className="bg-white md:max-w-[320px] max-w-[400px]"
           />
         </div>
 
         {/* 지역 버튼들 */}
-        <div className="flex flex-wrap justify-center md:justify-normal gap-4 md:gap-[18px]">
+        <div className="grid grid-cols-3 md:grid-cols-4 xl:grid-cols-8 gap-4 md:gap-[18px]">
           {regions.map((region) => (
             <button
               key={region}
               onClick={() => setSelectedRegion(region)}
               className={`flex items-center justify-center 
-        w-[153px] h-[60px] md:w-[204px] md:h-20 bg-white
+        h-[60px] md:h-20 bg-white w-full
         text-16r md:text-18r rounded-xl border transition-all
         hover:bg-main_bg2 hover:border-main
         ${
---
+++ app/(home)/content/scrapSection.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import more from "@/app/images/icon/more.svg";
+import { useRouter } from "next/navigation";
+import { agentMockData, expertMockData } from "@/app/data/mock/mockAgent";
+import ScrapProfileCard from "@/app/components/card/scrapProfileCard";
+
+const ScrapSection = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex w-full flex-col pb-10 md:pb-20 px-[15px] xl:pb-[120px] mt-[60px] md:mt-[100px]">
+      <div
+        className="flex flex-col gap-5 md:gap-0 md:flex-row
+      justify-between items-center mb-5 md:mb-[60px]"
+      >
+        <h2 className="text-24m md:text-36m text-text-primary text-center">
+          내가 찜한 <span className="text-main">중개사/전문가를</span>
+          만나보세요!
+        </h2>
+        <div
+          className="flex items-center cursor-pointer place-self-end md:place-self-stretch"
+          onClick={() => {
+            router.push("/scrap");
+          }}
+        >
+          <p className="text-text-secondary text-14m md:text-16m">더보기</p>
+          <Image src={more} alt={"더보기"} width={20} height={20} />
+        </div>
+      </div>
+      <h2 className="text-text-primary text-18s md:text-24s mb-5">
+        공인중개사
+      </h2>
+      <div className="flex flex-wrap gap-5 md:gap-10 w-max px-1 overflow-visible">
+        {agentMockData.map((expert) => (
+          <div key={expert.brokerId} className="shrink-0">
+            <ScrapProfileCard {...expert} />
+          </div>
+        ))}
+      </div>
+
+      <h2 className="text-text-primary text-18s md:text-24s mb-5 mt-10">
+        생활 서비스 전문가
+      </h2>
+      <div className="flex flex-wrap gap-5 md:gap-10 w-max px-1 overflow-visible">
+        {expertMockData.map((expert) => (
+          <div key={expert.brokerId} className="shrink-0">
+            <ScrapProfileCard {...expert} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ScrapSection;
